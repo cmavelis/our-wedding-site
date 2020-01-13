524ed34b5b89358d80855f9afb6471b4e07eaece
Cameron Avelis <cmavelis@gmail.com>
2020-01-12 22:57:37 -0500

    adding noindex nofollow

--- src/html.js
+++ src/html.js
@@ -13,6 +13,7 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <meta name="robots" content="noindex, nofollow" />
         <TypographyStyle typography={typography} />
         <GoogleFont typography={typography} />
         {props.headComponents}
