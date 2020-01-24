d1d5c398ebc905c32b3a834c42d22f8a444c136b
Cameron Avelis <cmavelis@gmail.com>
2020-01-23 22:33:32 -0500

    prototype of slide animation working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { graphql } from "gatsby"
 import {injectIntl, intlShape, FormattedMessage} from "react-intl";
 import { compose } from "recompose";
@@ -23,45 +23,60 @@ export const query = graphql`
   }
 `;
 
-const Index = ({ data }) => (
-    <React.Fragment>
-        <BackgroundSection className='save-the-date__background'>
+const Index = ({ data }) => {
+    const [slideUp, setSlideUp] = useState(false);
 
-        <div className='save-the-date__container'>
-            <header className='save-the-date__header'/>
-            <main className='save-the-date__main'>
-                <span className='save-the-date__floating-text'>Save the Date</span>
-                <section className='save-the-date__image-and-text'>
-                    <Img fixed={data.file.childImageSharp.fixed} />
+    const handleClick = () => {
+        setSlideUp(!slideUp);
+    };
 
-                    <div className='save-the-date__text-area'>
-                        <h1>
-                            <p>Lillian Nguyen</p>
-                            <p>&</p>
-                            <p>Cameron Avelis</p>
-                        </h1>
-                        <p>are getting married!</p>
-                        <br />
-                        <div className='save-the-date__text-area--information'>
-                            <FormattedMessage id="savethedate.date" tagName="div" />
-                            <p>Gertrude's</p>
-                            <p>10 Art Museum Dr.</p>
-                            <p>Baltimore, MD</p>
-                        </div>
-                        <br />
-                        <p>full website to follow with more details</p>
-                    </div>
-                </section>
-            </main>
-            {/*<footer className='save-the-date__footer'/>*/}
+    return(
+        <React.Fragment>
+            <BackgroundSection className='save-the-date__background'>
 
-            {/*<footer className='save-the-date__footer'>*/}
-            {/*    /!*<span>Background Image: Lillian Nguyen</span>*!/*/}
-            {/*</footer>*/}
-        </div>
-        </BackgroundSection>
-    </React.Fragment>
-);
+                <div className='save-the-date__container'>
+                    <header className='save-the-date__header'/>
+                    <main className='save-the-date__main'>
+                        <span className='save-the-date__floating-text'>Save the Date</span>
+                        <section className='save-the-date__image-and-text'>
+                            <Img fixed={data.file.childImageSharp.fixed} />
+
+                            <div className='save-the-date__text-area-wrapper'>
+                                <div className={'save-the-date__text-area' + (slideUp ? ' slide-up' : '')}>
+                                    <h1>
+                                        <p>Lillian Nguyen</p>
+                                        <p>&</p>
+                                        <p>Cameron Avelis</p>
+                                    </h1>
+                                    <p>are getting married!</p>
+                                    <br />
+                                    <div className='save-the-date__text-area--information'>
+                                        <FormattedMessage id="savethedate.date" tagName="div" />
+                                        <p>Gertrude's</p>
+                                        <p>10 Art Museum Dr.</p>
+                                        <p>Baltimore, MD</p>
+                                    </div>
+                                    <br />
+                                    <p>full website to follow with more details</p>
+                                    <button onClick={handleClick}> click </button>
+                                    <br />
+                                    <br />
+                                    <br />
+                                    <p>I'm hidden below</p>
+                                </div>
+                            </div>
+                        </section>
+                    </main>
+                    {/*<footer className='save-the-date__footer'/>*/}
+
+                    {/*<footer className='save-the-date__footer'>*/}
+                    {/*    /!*<span>Background Image: Lillian Nguyen</span>*!/*/}
+                    {/*</footer>*/}
+                </div>
+            </BackgroundSection>
+        </React.Fragment>
+    );
+};
 
 Index.propTypes = {
     intl: intlShape.isRequired
