162282c0577a891248503c5d5c1c2978ff3e8f26
Cameron Avelis <cmavelis@gmail.com>
2020-01-25 23:52:24 -0500

    hooks connected to form inputs

--- src/components/AddressForm/AddressForm.jsx
+++ src/components/AddressForm/AddressForm.jsx
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
 
 const AddressForm = () => {
+    const [email, setEmail] = useState('');
+    const [address, setAddress] = useState('');
+
     return(
     <form>
         <fieldset>
             <legend>Please input your contact information for invitation and updates</legend>
             <label htmlFor="">
-                email
+                email = {email}
             </label>
-            <input type="text"/>
+            <input type="text" onChange={e => setEmail(e.target.value)}/>
             <label htmlFor="">
-                address
+                address = {address}
             </label>
-            <input type="text"/>
+            <input type="text" onChange={e => setAddress(e.target.value)}/>
         </fieldset>
     </form>
     )
