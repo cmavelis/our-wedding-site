86e770717006c1d1ed22f757179d0b30e72402ee
Cameron Avelis <cmavelis@gmail.com>
2020-04-11 21:28:42 -0400

    RSVP blank until name field typed into

--- src/components/RSVPForm/rsvpFormReducer.js
+++ src/components/RSVPForm/rsvpFormReducer.js
@@ -14,14 +14,14 @@ export const RSVP_FIELD_ACTION_TYPES = {
 export const rsvpFormInitialState = [
     {
         name: '',
-        rsvp: RSVP_TYPES.YES,
+        rsvp: '',
     }
 ];
 
 export const rsvpFormReducer = (state, action) => {
     const oldIndex = action.index;
     const oldLength = state.length;
-    const newState = state;
+    const newState = [...state];
     switch (action.type) {
         case RSVP_FIELD_ACTION_TYPES.ADD:
             newState.push({
@@ -35,12 +35,13 @@ export const rsvpFormReducer = (state, action) => {
         case RSVP_FIELD_ACTION_TYPES.EDIT_NAME:
             newState[oldIndex].name = action.name;
             if (action.name === '') {
-                newState.splice(oldIndex, 1); //remove 1
+                newState.splice(oldIndex, 1); // remove 1
             } else if (oldIndex === oldLength-1) {
                 newState.push({
                     name: '',
-                    rsvp: RSVP_TYPES.YES,
-                });
+                    rsvp: '',
+                }); // add a blank entry
+                newState[oldIndex].rsvp = RSVP_TYPES.YES; // if first typing, default to YES
             }
             break;
         case RSVP_FIELD_ACTION_TYPES.EDIT_RSVP:
